59ddc39c466c293efe12dc30913e5c00aa69a663
unknown <sdlbsso@gmail.com>
2022-02-22 15:50:52 -0300

    pagina de login y redireccion

--- src/app.py
+++ src/app.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
 from config import config
 
 app=Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return render_template('auth/login.html')
+    return redirect(url_for('login'))
+
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method=='POST':
+        print('post ',request.form['username'])
+        print('post', request.form['password'])
+        return render_template('auth/login.html')
+    else:
+        return render_template('auth/login.html')
     
 
 
