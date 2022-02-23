acd9285968c5a5ab892413896b2b253b90ef30df
Sergio De Luca <sdlbsso@gmail.com>
2022-02-23 00:56:21 -0300

    login completo

--- src/app.py
+++ src/app.py
@@ -1,7 +1,22 @@
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, flash, redirect, render_template, request, url_for
+from flask_mysqldb import MySQL
+from flask_wtf.csrf import CSRFProtect
 from config import config
+from models.ModelUser import ModelUser
+from models.entities.User import User
+from flask_login import LoginManager, login_user, logout_user, login_required
 
 app=Flask(__name__)
+
+csrf=CSRFProtect()
+
+#Hace conexión con bd
+db = MySQL(app)
+login_manager_app = LoginManager(app)
+
+@login_manager_app.user_loader
+def load_user(id):
+    return ModelUser.get_by_id(db,id)
 
 
 @app.route('/')
@@ -12,15 +27,52 @@ def index():
 @app.route('/login', methods=['GET','POST'])
 def login():
     if request.method=='POST':
-        print('post ',request.form['username'])
-        print('post', request.form['password'])
-        return render_template('auth/login.html')
+        username = request.form['username']
+        password = request.form['password']
+        user = User(0,username,password)
+        logged_user=ModelUser.login(db,user)
+        if logged_user != None:
+            if logged_user.password:
+                login_user(logged_user)
+                return redirect(url_for('home'))
+            else:
+                flash("Datos incorrectos...")
+                return render_template('auth/login.html')
+        else:
+            flash("Usuario no encontrado...")
+            return render_template('auth/login.html')
     else:
         return render_template('auth/login.html')
-    
 
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+
+@app.route('/home')
+@login_required
+def home():
+    return render_template('home.html')
+
+
+@app.route('/protect')
+@login_required
+def protect():
+    return "<h1> Solo los autenticados pueden entrar acá </h1>"
+
+def status_401(error):
+    return redirect(url_for('login'))
+
+def status_404(error):
+    return "<h1>Página no encontrada</h1>"
 
 
 if __name__=='__main__':
     app.config.from_object(config['development'])
+    csrf.init_app(app)
+    app.register_error_handler(401,status_401)
+    app.register_error_handler(404,status_401)
     app.run()
--- src/config.py
+++ src/config.py
@@ -1,5 +1,15 @@
-class DevelopmentConfig():
+from flask import Config
+
+
+class Config:
+    SECRET_KEY = 'B!1weNAt1T^%kvhUI*S^'
+
+class DevelopmentConfig(Config):
     DEBUG=True
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = 'root'
+    MYSQL_DB = 'dbuser'
 
 config={
     'development' : DevelopmentConfig
