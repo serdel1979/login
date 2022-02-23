c5bc6806ca418c6f87f889b66e9f54b8008a293b
unknown <sdlbsso@gmail.com>
2022-02-23 12:48:04 -0300

    login

---
+++ src/tabla_users.sql
@@ -0,0 +1,2 @@
+INSERT INTO dbuser.users (username,password,fullname) VALUES
+	 ('admin','pbkdf2:sha256:260000$hjLrKxqjlDvwftkj$91c9ca16394726b5834f1625689655bf948688903a0607b03dbb8794332e7d01','administrador');
