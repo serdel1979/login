f8bdbde5a8727fc00377613f5e482cb79295b1a3
Sergio De Luca <sdlbsso@gmail.com>
2022-03-19 19:13:41 -0300

    base sql

---
+++ base.sql
@@ -0,0 +1,60 @@
+-- --------------------------------------------------------
+-- Host:                         localhost
+-- Versión del servidor:         10.3.31-MariaDB - mariadb.org binary distribution
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             9.4.0.5125
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Volcando estructura de base de datos para dbuser
+CREATE DATABASE IF NOT EXISTS `dbuser` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `dbuser`;
+
+-- Volcando estructura para tabla dbuser.solicitud_turnos
+CREATE TABLE IF NOT EXISTS `solicitud_turnos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
+  `id_vacuna` int(11) NOT NULL,
+  `vacuna` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `fecha_solicitud` date DEFAULT NULL,
+  `fecha_turno` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+-- La exportación de datos fue deseleccionada.
+-- Volcando estructura para tabla dbuser.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(150) NOT NULL,
+  `nombre` varchar(150) DEFAULT NULL,
+  `apellido` varchar(100) NOT NULL,
+  `dni` varchar(100) NOT NULL,
+  `telefono` varchar(100) NOT NULL,
+  `mail` varchar(100) NOT NULL,
+  `fecha_nacimiento` date NOT NULL,
+  `tiene_dosis` int(11) NOT NULL,
+  `factor_riesgo` int(11) NOT NULL,
+  `tipo` int(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
+
+-- La exportación de datos fue deseleccionada.
+-- Volcando estructura para tabla dbuser.vacunas
+CREATE TABLE IF NOT EXISTS `vacunas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `vacuna` varchar(100) NOT NULL,
+  `cantidad` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+-- La exportación de datos fue deseleccionada.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- src/tabla_users.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO dbuser.users (username,password,nombre) VALUES
-	 ('admin','pbkdf2:sha256:260000$hjLrKxqjlDvwftkj$91c9ca16394726b5834f1625689655bf948688903a0607b03dbb8794332e7d01','administrador');
